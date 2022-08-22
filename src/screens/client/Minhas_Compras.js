cb82a3533b19227d421ca6cf8d382665245604a1
teceudesenvolvo <79342596+teceudesenvolvo@users.noreply.github.com>
2022-08-22 16:30:49 -0300

    ok

--- src/screens/client/Minhas_Compras.js
+++ src/screens/client/Minhas_Compras.js
@@ -19,8 +19,6 @@ class Minhas_Compras extends Component{
 
             <p>Minhas Compras</p>
             <div className='minhasCompras'>
-                
-                  <p>Você é liso</p>
                   <p>Não Tem Compras!</p>
             </div>
            
