498eb2dbda8423b6f8ef77b8a0899feff7c042c9
teceudesenvolvo <79342596+teceudesenvolvo@users.noreply.github.com>
2022-08-24 10:10:09 -0300

    menuNovo

--- src/componets/menuDesktop.js
+++ src/componets/menuDesktop.js
@@ -22,11 +22,11 @@ class menuDesktop extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            linkMenu: 'linkMenu',
-            linkMenu2: 'linkMenu',
-            linkMenu3: 'linkMenu',
-            linkMenu4: 'linkMenu',
-            linkMenu5: 'linkMenu',
+            linkMenu: 'aDesktop',
+            linkMenu2: 'aDesktop',
+            linkMenu3: 'aDesktop',
+            linkMenu4: 'aDesktop',
+            linkMenu5: 'aDesktop',
             window: window.location.pathname,
         }
     }
@@ -35,15 +35,15 @@ class menuDesktop extends Component {
     btnHome = () => {
         switch (this.state.window) {
             case `/`:
-                return this.setState({ linkMenu: 'linkMenu link-active' })
+                return this.setState({ linkMenu: 'aDesktop link-desktop-active' })
             case `/favoritos`:
-                return this.setState({ linkMenu2: 'linkMenu link-active' })
+                return this.setState({ linkMenu2: 'aDesktop link-desktop-active' })
             case `/Compras`:
-                return this.setState({ linkMenu3: 'linkMenu link-active' })
+                return this.setState({ linkMenu3: 'aDesktop link-desktop-active' })
             case `/Notificacoes`:
-                return this.setState({ linkMenu4: 'linkMenu link-active' })
+                return this.setState({ linkMenu4: 'aDesktop link-desktop-active' })
             case `/Mais`:
-                return this.setState({ linkMenu5: 'linkMenu link-active' })
+                return this.setState({ linkMenu5: 'aDesktop link-desktop-active' })
             default:
                 return null
         }
@@ -64,30 +64,30 @@ class menuDesktop extends Component {
 
                 <a href='/' className="logoDesktop" >
                     <img src={Logo}  ></img>
-                    <h1 className='h1-logo'>| List</h1>
+                    {/* <h1 className='h1-logo'>| List</h1> */}
                 </a>
 
-                <a href="/" className="aDesktop">
+                <a href="/" className={this.state.linkMenu}>
                     <FaHome className='fas fa-home'></FaHome>
                     <span className='nav-item'>Home</span>
                 </a>
 
-                <a href="/favoritos" className="aDesktop">
+                <a href="/favoritos" className={this.state.linkMenu2}>
                     <FaHeart className='fas fa-favoritos'></FaHeart>
                     <span className='nav-item'>favoritos</span>
                 </a>
 
-                <a href="/Compras" className="aDesktop">
+                <a href="/Compras" className={this.state.linkMenu3}>
                     <FaShoppingBag className='fas fa-Compras'></FaShoppingBag>
                     <span className='nav-item'>Compras</span>
                 </a>
 
-                <a href="/Notificacoes" className="aDesktop">
+                <a href="/Notificacoes" className={this.state.linkMenu4}>
                     <FaBell className='fas fa-Notificacoes'></FaBell>
                     <span className='nav-item'>Notificações</span>
                 </a>
 
-                <a href="/Mais" className="aDesktop">
+                <a href="/Mais" className={this.state.linkMenu5}>
                     <FaBars className='fas fa-Mais'></FaBars>
                     <span className='nav-item'>Mais</span>
                 </a>
