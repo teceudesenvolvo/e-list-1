a0f3f123699b488615e07c836d3b7632509c7976
teceudesenvolvo <79342596+teceudesenvolvo@users.noreply.github.com>
2022-08-25 12:07:59 -0300

    produto

--- src/screens/client/Produto.js
+++ src/screens/client/Produto.js
@@ -7,7 +7,6 @@ import camera from '../../assets/Camera.png';
 // Icones
 
 // Components
-import Menu from '../../componets/menu';
 
 //mudança de páginas
 
@@ -19,24 +18,15 @@ class Produto extends Component {
 
 
         <div className='Produto-container'>
-          <p className='align-left'>Mais Vendido</p>
-          <h3 className='Produto-title align-left'>Lavagem Completa</h3>
-          <div className='favoritosItem'>
+          <p className='textoDestaques'>Mais Vendido</p>
+          <h1 className='Produto-title'>Lavagem Completa</h1>
+          <div className='productId'>
             <img src={camera} alt='camera' className='camera' />
-          </div>
-          <input type='text'className='inputLogin' name='select-Produto' placeholder='Escreva uma obisevação'/>
-
-
-        
-         
-
-          <ul className='align-left'>
-            <li>Lavagem de pintura</li>
-            <li>Aspiração carpetes</li>
-            <li>Limpeza de painel</li>
-            <li>Aspiração de bancos</li>
-            <li>Enceramento de pintura</li>
-          </ul>
+          </div>    
+          <h1 className='servico-desc'>Descrição do serviço </h1>
+          <p className='txtProduct'>
+            Lorem
+          </p>
            <div >
             <h5 >R$ 60,00</h5><button>Confira Disponibilidade</button>
            </div>
