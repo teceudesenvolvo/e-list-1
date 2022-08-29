f85cf791cd83b39eaf4c813f62979d67b954a02c
FELIPE RODRIGUES DE MELO <85718153+liperds@users.noreply.github.com>
2022-08-29 12:16:36 -0300

    pagamento

--- src/screens/client/pagamento.js
+++ src/screens/client/pagamento.js
@@ -3,22 +3,37 @@ import React, { Component } from 'react';
 //Imagens
 import camera from '../../assets/Camera.png';
 // Icones
+import {
 
+    FaStar
+} from 'react-icons/fa';
 // Components
 
 //mudança de páginas
 
 class pagamento extends Component {
-  render() {
-    return (
+    render() {
+        return (
 
-      <div className='App-header' >
+            <div className='App-header' >
+                <div className='Produto-container'>
+                    
+                    <p>Realizar Pagamento</p>
 
-        <p>Pagamento</p>
-       
-      </div>
-    );
-  }
+
+                    <div className='price-buttom' >
+                        <div >
+                            <h5 >R$ 60,00</h5>
+                            <h5 > <FaStar color='#FF7A00' /> 4,9</h5>
+                        </div>
+
+                        <input type='button' value="Concluir" />
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default pagamento;
