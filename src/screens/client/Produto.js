38751c0a5556f554e53f197a149fe5674b6a43f3
teceudesenvolvo <79342596+teceudesenvolvo@users.noreply.github.com>
2022-08-29 12:20:03 -0300

    meus

--- src/screens/client/Produto.js
+++ src/screens/client/Produto.js
@@ -1,35 +1,27 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
+import React from 'react';
 
 
 //Imagens
 import camera from '../../assets/Camera.png';
 // Icones
 import {
-  FaColumns,
   FaStar
 } from 'react-icons/fa'
 // Components
 import CustomizeDayPicker from "../../componets/CustomizeDayPicker"
 
-import Menu from '../../componets/menu';
 
 //mudança de páginas
 
 
-import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
-import ListItemText from '@mui/material/ListItemText';
-import ListItem from '@mui/material/ListItem';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import CloseIcon from '@mui/icons-material/Close';
 import Slide from '@mui/material/Slide';
-import { Rowing } from '@mui/icons-material';
 
 
 
