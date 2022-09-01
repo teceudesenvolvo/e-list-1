4017cc94d487caa6e1ff4886aa5f7185b2608c72
FELIPE RODRIGUES DE MELO <85718153+liperds@users.noreply.github.com>
2022-09-01 13:43:22 -0300

    pagamento

--- src/screens/client/pagamento.js
+++ src/screens/client/pagamento.js
@@ -17,12 +17,33 @@ class pagamento extends Component {
 
             <div className='App-header' >
                 <div className='Pagamento-container'>
-                <FaCheckCircle className='ok-circle'></FaCheckCircle>
+                    <FaCheckCircle className='ok-circle'></FaCheckCircle>
                     <h5>Pagamento Realizado Com Sucesso</h5>
-                    
-                   
-                
-                   
+                    <input className='ok-input'
+                        onClick={
+                            () => {
+                                window.location.href = "/Compras"
+                                // this.setState({id: aviso.id}, () => {
+                                // (this.props.clickButton(this.state))
+                                //   }
+                            }
+                        }
+
+                        type='button' value="Ver Conpovante" />
+                    <input className='ok-input'
+                        onClick={
+                            () => {
+                                window.location.href = "/Compras"
+                                // this.setState({id: aviso.id}, () => {
+                                // (this.props.clickButton(this.state))
+                                //   }
+                            }
+                        }
+
+                        type='button' value="Ver minhas Compras " />
+
+
+
                 </div>
 
             </div>
