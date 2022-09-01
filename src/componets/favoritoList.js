1d340fe954fe335a0ff3d2aadb875a522169be4b
FELIPE RODRIGUES DE MELO <85718153+liperds@users.noreply.github.com>
2022-09-01 10:40:35 -0300

    Front 60% ok

--- src/componets/favoritoList.js
+++ src/componets/favoritoList.js
@@ -16,49 +16,59 @@ class favoritoList extends Component {
                 id: '1',
                 image: 'https://utilider.com/wp-content/uploads/2022/04/BATERIA-ALFACELL-LITHIUM-3V-CARTELA-C-2.webp',
                 desc: 'Descrição do serviço 1',
-                value: '192,50' 
+                value: '192,50', 
+                valueP: '2x R$ 96,25' 
             }, 
             {
                 id: '1',
                 image: 'https://utilider.com/wp-content/uploads/2022/04/PILHA-ALFACELL-COMUM-PQ-AA-1.5V-C-4-CARTELA.webp',
                 desc: 'Descrição do serviço 1',
-                value: '192,50' 
+                value: '192,50',
+                valueP: '2x R$ 96,25' 
+
             }, 
             {
                 id: '1',
                 image: 'https://utilider.com/wp-content/uploads/2022/04/PISTOLA-PCOLA-QUENTE.webp',
                 desc: 'Descrição do serviço 1',
-                value: '192,50' 
+                value: '192,50',
+                valueP: '2x R$ 96,25' 
             }, 
             {
                 id: '1',
                 image: 'https://utilider.com/wp-content/uploads/2022/04/VDA06020-PETISQUEIRA-DE-VIDRO-D.webp',
                 desc: 'Descrição do serviço 1',
-                value: '192,50' 
+                value: '192,50',
+                valueP: '2x R$ 96,25'  
             }, 
             {
                 id: '1',
                 image: 'https://utilider.com/wp-content/uploads/2022/04/POTE-C-TAMPA-CLEAN-PRA.jpg',
                 desc: 'Descrição do serviço 1',
-                value: '192,50' 
+                value: '192,50',
+                valueP: '2x R$ 96,25' 
             }, 
             {
                 id: '1',
                 image: 'https://utilider.com/wp-content/uploads/2022/04/tabua-bambu-nv.webp',
                 desc: 'Descrição do serviço 1',
-                value: '192,50' 
+                value: '192,50',
+                valueP: '2x R$ 96,25'  
             }, 
             {
                 id: '1',
                 image: 'https://utilider.com/wp-content/uploads/2022/04/JARRA-DE-VIDRO-LOSANGO-416x416.jpg',
                 desc: 'Descrição do serviço 1',
-                value: '192,50' 
+                value: '192,50',
+                valueP: '2x R$ 96,25'  
             }, 
             {
                 id: '1',
                 image: 'https://utilider.com/wp-content/uploads/2022/04/POTE-DE-VIDRO-RT-C-DIV.jpg',
                 desc: 'Descrição do serviço que vai',
-                value: '192,50' 
+                value: '192,50',
+                valueP: '2x R$ 96,25' 
+
             }, 
         ]
     }
@@ -87,6 +97,7 @@ class favoritoList extends Component {
                 <img alt='produto favorito' src={product.image} width="50%" className='imgProduct'/>
             <div className='areaTextDescProduct' >
                 <p className='valueProduct' >R$ {product.value}</p>
+                <p className='valueProductP' >{product.valueP}</p>
                 <p className='descricaoProduct' >{product.desc}</p>
             </div>
       </li>
