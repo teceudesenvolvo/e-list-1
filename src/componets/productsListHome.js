0d58398e0f7d98a9b02bbcdd2c47b2b58d7edaf2
FELIPE RODRIGUES DE MELO <85718153+liperds@users.noreply.github.com>
2022-09-06 09:51:30 -0300

    serviços

--- src/componets/productsListHome.js
+++ src/componets/productsListHome.js
@@ -20,20 +20,20 @@ class ProductsList extends Component {
             }, 
             {
                 id: '1',
-                image: 'https://utilider.com/wp-content/uploads/2022/04/PILHA-ALFACELL-COMUM-PQ-AA-1.5V-C-4-CARTELA.webp',
-                desc: 'Descrição do serviço 1',
+                image: 'https://autolider-ok6fhsopc-felipe00007.vercel.app/img/motor.jpg',
+                desc: 'Limpeza detalhada de motor',
                 value: '192,50' 
             }, 
             {
                 id: '1',
-                image: 'https://utilider.com/wp-content/uploads/2022/04/PISTOLA-PCOLA-QUENTE.webp',
-                desc: 'Descrição do serviço 1',
+                image: 'https://autolider-ok6fhsopc-felipe00007.vercel.app/img/rodas.jpg',
+                desc: 'Limpeza detalhada Em Rodas',
                 value: '192,50' 
             }, 
             {
                 id: '1',
-                image: 'https://utilider.com/wp-content/uploads/2022/04/VDA06020-PETISQUEIRA-DE-VIDRO-D.webp',
-                desc: 'Descrição do serviço 1',
+                image: 'https://autolider-ok6fhsopc-felipe00007.vercel.app/img/moto.jpg',
+                desc: 'Limpeza Em Motocicletas',
                 value: '192,50' 
             }, 
             {
