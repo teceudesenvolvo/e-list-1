41314d04b6789a331bf2fac4caa44f0afb56e660
FELIPE RODRIGUES DE MELO <85718153+liperds@users.noreply.github.com>
2022-09-12 10:52:28 -0300

    css

--- src/screens/client/register.js
+++ src/screens/client/register.js
@@ -10,29 +10,32 @@ import logo from '../../assets/logoLaranga.png';
 //mudança de páginas
 
 
-class register extends Component{
-  render(){  
-    return (      
+class register extends Component {
+  render() {
+    return (
+      <div className='App-header' >
         <div className='Container' >
-            <img src={logo} alt="logo" className='logo'/>
-            <h1>Seja bem-vindo!</h1>
-            <form className='formLogin'>
-                <input type="text" placeholder="Nome" className='inputLogin' />
-                <input type="text" placeholder="CPF" className='inputLogin'/>
-                <input type="text" placeholder="E-mail" className='inputLogin' />
-                <input type="password" placeholder="Senha" className='inputLogin'/>
-                <input type="password" placeholder="Confirmar senha" className='inputLogin'/>
-                <input type="text" placeholder="Telefone" className='inputLogin'/>
-                <input type="text" placeholder="CEP" className='inputLogin'/>
-                <input type="text" placeholder="Número da casa" className='inputLogin'/>
-                <input type="checkbox" placeholder="Complemento" className='inputLogin'/>
-                <button className='buttonLogin'>Cadastrar</button>
+          <img src={logo} alt="logo" className='logo' />
+          <h1>Seja bem-vindo!</h1>
+          <form className='formLogin'>
+            <input type="text" placeholder="Nome" className='inputLogin' />
+            <input type="text" placeholder="CPF" className='inputLogin' />
+            <input type="text" placeholder="E-mail" className='inputLogin' />
+            <input type="password" placeholder="Senha" className='inputLogin' />
+            <input type="password" placeholder="Confirmar senha" className='inputLogin' />
+            <input type="text" placeholder="Telefone" className='inputLogin' />
+            <input type="text" placeholder="CEP" className='inputLogin' />
+            <input type="text" placeholder="Número da casa" className='inputLogin' />
+            <input type="checkbox" placeholder="Complemento" className='inputLogin' />
+            <button className='buttonLogin'>Cadastrar</button>
 
-            </form> <p>já tem uma conta? <a href='/login' className='linkLogin'>Fazer login</a></p>
-            
+          </form> <p>já tem uma conta? <a href='/login' className='linkLogin'>Fazer login</a></p>
+
         </div>
+
+      </div>
     );
   }
-}            
+}
 
 export default register;
