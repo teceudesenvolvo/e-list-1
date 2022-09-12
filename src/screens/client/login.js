6839fb25f0c956575238cf88b864ce6d6d5e2b1d
FELIPE RODRIGUES DE MELO <85718153+liperds@users.noreply.github.com>
2022-09-12 10:47:46 -0300

    login

--- src/screens/client/login.js
+++ src/screens/client/login.js
@@ -11,31 +11,33 @@ import face from '../../assets/face.png';
 
 //mudança de páginas
 
-class loginClient extends Component{
-  render(){  
-    return (      
-        <div className='Container' >
-            <img src={logo} alt="logo" className='logo'/>
-            <h1>Seja bem-vindo!</h1>
-            <form className='formLogin'>
-                <input type="text" placeholder="E-mail" className='inputLogin' />
-                <input type="password" placeholder="Senha" className='inputLogin'/>
-                <a href='#' className='linkLogin'>Esqueceu a senha?</a>
-                <button className='buttonLogin'>Entrar</button>
-            </form>
-            <p>Entre com sua conta:</p>
-            <div className='divLogin'>
-                <a href='#' className='logoGoogle'>
-                    <img src={google} alt="google" className='imgLogin'/>
-                </a>
-                <a href='#' className='logoFace'>
-                    <img src={face} alt="face" className='imgLogin'/>
-                </a>
+class loginClient extends Component {
+    render() {
+        return (
+            <div className='App-header' >
+                <div className='Container' >
+                    <img src={logo} alt="logo" className='logo' />
+                    <h1>Seja bem-vindo!</h1>
+                    <form className='formLogin'>
+                        <input type="text" placeholder="E-mail" className='inputLogin' />
+                        <input type="password" placeholder="Senha" className='inputLogin' />
+                        <a href='#' className='linkLogin'>Esqueceu a senha?</a>
+                        <button className='buttonLogin'>Entrar</button>
+                    </form>
+                    <p>Entre com sua conta:</p>
+                    <div className='divLogin'>
+                        <a href='#' className='logoGoogle'>
+                            <img src={google} alt="google" className='imgLogin' />
+                        </a>
+                        <a href='#' className='logoFace'>
+                            <img src={face} alt="face" className='imgLogin' />
+                        </a>
+                    </div>
+                    <p>Não tem uma conta? <a href='/register' className='linkLogin'>Crie uma</a></p>
+                </div>
             </div>
-            <p>Não tem uma conta? <a href='/register' className='linkLogin'>Crie uma</a></p>
-        </div>
-    );
-  }
-}            
+        );
+    }
+}
 
 export default loginClient;
