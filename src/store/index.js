42c78e8bba58f74560a79c9c7ada4feafa00e954
teceudesenvolvo <79342596+teceudesenvolvo@users.noreply.github.com>
2022-09-21 14:25:18 -0300

    ok

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, 
+    // applyMiddleware 
+} from 'redux';
 import {persistStore, persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 // import thunk from 'redux-thunk'
