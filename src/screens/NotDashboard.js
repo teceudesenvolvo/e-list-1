1c1eb9a122d718ee44429b0406aa153ccf6922c5
FELIPE RODRIGUES DE MELO <85718153+liperds@users.noreply.github.com>
2022-09-24 14:23:27 -0300

    css

--- src/screens/NotDashboard.js
+++ src/screens/NotDashboard.js
@@ -27,6 +27,7 @@ class NotDashboard extends Component {
 
                 </div>
                 <div className='conteinar-list-Products'>
+                    
                     <div>
                         <div className='conteinar-list-items'>
                             <div>
@@ -40,6 +41,39 @@ class NotDashboard extends Component {
 
                         </div>
 
+                        <div className='conteinar-list-items'>
+                            <div>
+                                <h1 className='Serviço-order-h1'>Nome do Cliente</h1>
+                                <h1 className='Serviço-order-h2'>Agendado às 12:10</h1>
+                            </div>
+                            <div className='not-Status'>
+                                <h5 className='Serviço-order-h1'>No horário</h5>
+                                
+                            </div>
+
+                        </div>
+                        <div className='conteinar-list-items'>
+                            <div>
+                                <h1 className='Serviço-order-h1'>Nome do Cliente</h1>
+                                <h1 className='Serviço-order-h2'>Agendado às 12:10</h1>
+                            </div>
+                            <div className='not-Status'>
+                                <h5 className='Serviço-order-h1'>No horário</h5>
+                                
+                            </div>
+
+                        </div>
+                        <div className='conteinar-list-items'>
+                            <div>
+                                <h1 className='Serviço-order-h1'>Nome do Cliente</h1>
+                                <h1 className='Serviço-order-h2'>Agendado às 12:10</h1>
+                            </div>
+                            <div className='not-Status'>
+                                <h5 className='Serviço-order-h1'>No horário</h5>
+                                
+                            </div>
+
+                        </div>
                         <div className='conteinar-list-items'>
                             <div>
                                 <h1 className='Serviço-order-h1'>Nome do Cliente</h1>
